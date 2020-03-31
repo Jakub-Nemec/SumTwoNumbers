ac729c5ce82bbf5352113d0dbca82f5b511a71d7
Jakub Němec <Jakub.K.Nemec@gmail.com>
2020-03-31 11:29:25 +0200

    Sum two numbers

---
+++ Úkoly/EmptyClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Úkoly
+{
+    class Class1
+    {
+
+        private double a;
+        private double b;
+        /*
+        //konstruktor
+        public Class1(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        */
+
+        public void Vystup()
+        {
+            Console.WriteLine("Zadejte první číslo:");
+            double x;
+            while (!double.TryParse(Console.ReadLine(), out x)){
+                Console.WriteLine("Špatně zadejte znova:");
+            }
+
+            Console.WriteLine("Zadejte druhé číslo:");
+            double y;
+            while (!double.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("Špatně zadejte znova:");
+            }
+
+            Console.WriteLine("Součet: {0}", Soucet(x,y));
+        }
+
+        private double Soucet(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Úkoly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Úkoly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            class1.Vystup();
+
+            //Console.WriteLine("Součet je {0}.", class1.Soucet(5, 6));
+
+            Console.ReadKey();
+          
+        }
+    }
+}
